d858d3a9db661efee5384459745375fc9603d8db
Robert Grancsa <robert.grancsa@softwire.com>
2025-04-08 20:33:47 +0300

    Specified host
    Signed-off-by: Robert Grancsa <robert.grancsa@softwire.com>

--- src/run.py
+++ src/run.py
@@ -1,6 +1,10 @@
 from app import create_app
+import os
 
 app = create_app()
 
+HOST = os.environ.get('HOST', '0.0.0.0')
+PORT = int(os.environ.get('PORT', '5000'))
+
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host=HOST, port=PORT, debug=True)
